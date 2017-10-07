c9979bcf8487410a373ff10e98824689c239fd54
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2017-10-07 20:39:42 +0300

    add header lenses

--- src/htstream.erl
+++ src/htstream.erl
@@ -38,6 +38,11 @@
   ,encode/1
   ,encode/2 
 ]).
+-export([
+   http_accept/0,
+   http_connection/0,
+   http_content_type/0
+]).
 
 -export_type([
    method/0, url/0, header/0, request/0, payload/0, http/0
@@ -961,12 +966,105 @@ is_payload_entity(S) ->
          end
    end. 
 
-is_payload_eof(S) ->
+is_payload_eof(#http{headers = Head} = State) ->
    %% Note: this routine makes a final statement if the request carries payload or not
-   case lists:keyfind(?HTTP_CONNECTION, 1, S#http.headers) of
-      {?HTTP_CONNECTION, <<"close">>} ->
-         {ok, S#http{is = eoh, length = inf}};
+   case lens:get(http_connection(), Head) of
+      <<"close">> ->
+         {ok, State#http{is = eoh, length = inf}};
       _ ->
          % look like http request / response go not carry on any payload
-         {ok, S#http{is = eoh, length = none}}
+         {ok, State#http{is = eoh, length = none}}
    end.
+
+%%%------------------------------------------------------------------
+%%%
+%%% http lenses
+%%%
+%%%------------------------------------------------------------------
+
+-define(lens(X), lens:pair(X, undefined)).
+
+http_accept() -> ?lens(<<"Accept">>).
+% -define(HTTP_  ,<<"Accept-Charset">>).
+% -define(HTTP_  ,<<"Accept-Datetime">>).
+% -define(HTTP_  ,<<"Accept-Encoding">>).
+% -define(HTTP_  ,<<"Accept-Language">>).
+% -define(HTTP_  ,<<"Access-Control-Request-Method">>).
+% -define(HTTP_  ,<<"Authorization">>).
+% -define(HTTP_  ,<<"Cache-Control">>).
+http_connection() -> ?lens(<<"Connection">>).
+% -define(HTTP_CONNECTION  ,<<"Connection">>).
+% -define(HTTP_  ,<<"Content-Length">>).
+% -define(HTTP_  ,<<"Content-MD5">>).
+http_content_type() -> ?lens(<<"Content-Type">>).
+% -define(HTTP_  ,<<"Cookie">>).
+% -define(HTTP_  ,<<"Date">>).
+% -define(HTTP_  ,<<"Expect">>).
+% -define(HTTP_  ,<<"Forwarded">>).
+% -define(HTTP_  ,<<"From">>).
+% -define(HTTP_HOST  ,<<"Host">>).
+% -define(HTTP_  ,<<"If-Match">>).
+% -define(HTTP_  ,<<"If-Modified-Since">>).
+% -define(HTTP_  ,<<"If-None-Match">>).
+% -define(HTTP_  ,<<"If-Range">>).
+% -define(HTTP_  ,<<"If-Unmodified-Since">>).
+% -define(HTTP_  ,<<"Max-Forwards">>).
+% -define(HTTP_  ,<<"Origin">>).
+% -define(HTTP_  ,<<"Pragma">>).
+% -define(HTTP_  ,<<"Proxy-Authorization">>).
+% -define(HTTP_  ,<<"Range">>).
+% -define(HTTP_  ,<<"Referer">>).
+% -define(HTTP_  ,<<"TE">>).
+% -define(HTTP_  ,<<"Upgrade">>).
+% -define(HTTP_  ,<<"User-Agent">>).
+% -define(HTTP_  ,<<"Via">>).
+% -define(HTTP_  ,<<"Warning">>).
+
+% -define(HTTP_  ,<<"Accept-Patch">>).
+% -define(HTTP_  ,<<"Accept-Ranges">>).
+% -define(HTTP_  ,<<"Access-Control-Allow-Origin">>).
+% -define(HTTP_  ,<<"Access-Control-Allow-Credentials">>). 
+% -define(HTTP_  ,<<"Access-Control-Expose-Headers">>).
+% -define(HTTP_  ,<<"Access-Control-Max-Age">>).
+% -define(HTTP_  ,<<"Access-Control-Allow-Methods">>). 
+% -define(HTTP_  ,<<"Access-Control-Allow-Headers">>).
+% -define(HTTP_  ,<<"Age">>).
+% -define(HTTP_  ,<<"Allow">>).
+% -define(HTTP_  ,<<"Alt-Svc">>).
+% -define(HTTP_  ,<<"Cache-Control">>).
+% -define(HTTP_  ,<<"Connection">>).
+% -define(HTTP_  ,<<"Content-Disposition">>).
+% -define(HTTP_  ,<<"Content-Encoding">>).
+% -define(HTTP_  ,<<"Content-Language">>).
+% -define(HTTP_CONTENT_LENGTH, <<"Content-Length">>).
+% -define(HTTP_  ,<<"Content-Location">>).
+% -define(HTTP_  ,<<"Content-MD5">>).
+% -define(HTTP_  ,<<"Content-Range">>).
+% -define(HTTP_  ,<<"Content-Type">>).
+% -define(HTTP_  ,<<"Date">>).
+% -define(HTTP_  ,<<"ETag">>).
+% -define(HTTP_  ,<<"Expires">>).
+% -define(HTTP_  ,<<"Last-Modified">>).
+% -define(HTTP_  ,<<"Link">>).
+% -define(HTTP_  ,<<"Location">>).
+% -define(HTTP_  ,<<"P3P">>).
+% -define(HTTP_  ,<<"Pragma">>).
+% -define(HTTP_  ,<<"Proxy-Authenticate">>).
+% -define(HTTP_  ,<<"Public-Key-Pins">>).
+% -define(HTTP_  ,<<"Retry-After">>).
+% -define(HTTP_  ,<<"Server">>).
+% -define(HTTP_  ,<<"Set-Cookie">>).
+% -define(HTTP_  ,<<"Strict-Transport-Security">>).
+% -define(HTTP_  ,<<"Tk">>).
+% -define(HTTP_  ,<<"Trailer">>).
+% -define(HTTP_TRANSFER_ENCODING, <<"Transfer-Encoding">>).
+% -define(HTTP_TRANSFER_LENGTH, <<"Transfer-Length">>).
+% -define(HTTP_  ,<<"Upgrade">>).
+% -define(HTTP_  ,<<"Vary">>).
+% -define(HTTP_  ,<<"Via">>).
+% -define(HTTP_  ,<<"Warning">>).
+% -define(HTTP_  ,<<"WWW-Authenticate">>).
+% -define(HTTP_  ,<<"X-Frame-Options">>).
+
+
+
